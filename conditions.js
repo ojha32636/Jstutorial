8377ee641b018332a27ab1b54865f1bfdf937098
unknown <ojha32636@gmail.com>
2026-01-13 19:12:41 +0530

    js training first commit

---
+++ conditions.js
@@ -0,0 +1,23 @@
+// here we will write program based on condition
+
+// program to identify eligibility to forcast the vote
+
+let age =18;
+
+if(age >= 18){
+
+    console.log("Eligible");
+}
+
+
+else if(age <= 10){
+
+    console.log("minor");
+}
+
+
+else{
+    console.log("not eligible");
+}
+
+// programming using switch 
---
+++ print.js
@@ -0,0 +1,26 @@
+// will print the hello world
+//console.log("Hello world");
+
+
+// We will find the area of a circle where the radius is 3.14cm
+
+let r= 3.14, pi= 3.14, area;
+
+area = 2*pi*r;
+
+//console.log(area);
+
+
+
+// calculator add subtract devide multiply
+
+let a = 5, b = 10, c = 2.75, sum, sub, prod, devide;
+
+sum = (a+b+c);
+
+sub = b-(a+c);
+
+prod = a*b*c;
+
+console.table([sum, sub, prod]);
+
