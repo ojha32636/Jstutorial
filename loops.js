48d5ef8d8893750a7d9c12ad90bb0db3ab35428d
unknown <ojha32636@gmail.com>
2026-01-15 18:27:22 +0530

    added more program

--- loops.js
+++ loops.js
@@ -29,7 +29,7 @@ for(let i=1; i<=5; i++){
 }
 
 
-// sweet print
+// Diamond print
 
 let n = 11;
 for(let i= 1; i <=n; i++){
@@ -42,3 +42,70 @@ for(let i=n-1; i>=1;i--){
     console.log(" ".repeat(n-i) + '*'.repeat(2*i-1));
 
 }
+
+// print all even numbers from 1 to 50.
+
+let num = 50;
+
+for (let i=1; i<=num; i++){
+
+    if (i%2 == 0){
+
+        console.log(i);
+    }
+}
+
+// Sum of first N numbers
+
+let x = 50, s=0;
+
+for(let i=1;i<=x;i++){
+
+    s += i;
+}
+console.log (s); 
+
+
+// multiplication of 5
+
+let t=5, ta;
+
+for(let i=1; i<=10; i++){
+
+    ta= i*t;
+
+    console.log(ta);
+
+}
+
+//same program using while loop
+let i =1;
+while(i<=10){
+
+    ta =i*t;
+
+    console.log(ta);
+i++;
+}
+
+// reverse counting using while loop
+
+let r = 10;
+while(r>=1){
+
+    console.log(r);
+
+    r--;
+}
+
+// find factorial
+
+let fact = 5, sol = 1;
+
+for(let i=fact; i>=1; i--){
+
+    sol = sol*i;
+    
+    
+}
+console.log(sol);
